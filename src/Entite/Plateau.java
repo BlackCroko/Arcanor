e09991c5cf0b78bade5535d6fb8bde49f0fec60c
adoublard <31312065+adoublard@users.noreply.github.com>
2018-03-28 00:32:20 +0200

    test

--- src/Entite/Plateau.java
+++ src/Entite/Plateau.java
@@ -5,7 +5,8 @@ public class Plateau {
 	Pion[][] grille = new Pion[7][8]; 
 	
 	public Plateau(){
-		System.out.print(grille.length+" and "+grille[0].length);
+		System.out.println(grille.length+" and "+grille[0].length);
+		System.out.println("coucou c'est abdoul");
 	}
 
 }
