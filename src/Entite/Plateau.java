46028be87bd329a8584a031ac9db917abc2690ff
adoublard <31312065+adoublard@users.noreply.github.com>
2018-03-28 01:02:37 +0200

    test

--- src/Entite/Plateau.java
+++ src/Entite/Plateau.java
@@ -1,13 +1,20 @@
 package Entite;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
-public class Plateau {
+public class Plateau implements EventHandler<MouseEvent> {
 	
-	Pion[][] grille = new Pion[7][8];
+	Case[][] grille = new Case[7][8];
 	int decalage;
 	int tailleCase;
 	int decalageTrait;
+	Pion pionSelectionne;
 	
 	public Plateau(int decalage, int tailleCase, int decalageTrait){
 		this.decalage = decalage;
@@ -23,9 +30,10 @@ public class Plateau {
 		// dessin des lignes
 		for (int i = 0; i < grille.length; i++) {
 			for (int j = 0; j < grille[i].length; j++) {
-					trait = new Case(decalage + tailleCase * i, decalage + decalageTrait + tailleCase * j,
+					grille[i][j] = new Case(decalage + tailleCase * i, decalage + decalageTrait + tailleCase * j,
 							tailleCase, tailleCase, i, j);
-					ajoutTrait(trait, i, j, false, troupe);
+					troupe.getChildren().add(trait);
+					grille[i][j].setOnMouseClicked(this);
 				
 			}
 		}
@@ -40,20 +48,118 @@ public class Plateau {
 
 	}
 	
-	void ajoutTrait(Case trait, int i, int j, boolean horizontal, Group troupe) {
-		troupe.getChildren().add(trait);
-		grille.add(trait);
-		trait.setOnMouseClicked(this);
-
-	}
-	
 	void ajoutPion(Pion pion, int i, int j, boolean horizontal, Group troupe) {
 		pion.setOnMouseClicked(this);
-        for(Case test : grille){
-        	if(test.getLigne() == pion.getLigne() && test.getCol() == pion.getCol())
-        	test.placePion();
-        }
+		grille[pion.getLigne()][pion.getCol()].placePion();
 		troupe.getChildren().add(pion);
 	}
+
+
+	public void handle(MouseEvent me){
+		  
+		  Object o = me.getSource();
+		  if(o instanceof Pion)
+		  {
+		    Pion d = (Pion)o;
+
+	        System.out.println("clic en " + d.getLigne() + "," + d.getCol());
+		    //si on selectionne un nouveau pion ou deselectionne le pion deja selectionne
+		    if(pionSelectionne==null || pionSelectionne==d)
+		    {
+		      d.switchSelected();
+		      d.colorPion();
+		      if(pionSelectionne==null){ 
+		    	  pionSelectionne=d;
+		    	  int x = pionSelectionne.getLigne();
+		    	  int y = pionSelectionne.getCol();
+		    	  for(Case test : grille){
+		    		  if(test.getLigne() == x+1 && test.getCol() == y+1){
+		    			  if(!test.isPlace())
+		    			  test.ispossible();
+		    			  else test.isimpossible();
+		    		  }
+		    		  if(test.getLigne() == x && test.getCol() == y+1){
+		    			  if(!test.isPlace())
+		    			  test.ispossible();
+		    			  else test.isimpossible();
+		    		  }
+		    		  if(test.getLigne() == x-1 && test.getCol() == y+1){
+		    			  if(!test.isPlace())
+		    			  test.ispossible();
+		    			  else test.isimpossible();
+		    		  }
+		    		  if(test.getLigne() == x+1 && test.getCol() == y){
+		    			  if(!test.isPlace())
+		    			  test.ispossible();
+		    			  else test.isimpossible();
+		    		  }
+		    		  if(test.getLigne() == x+1 && test.getCol() == y-1){
+		    			  if(!test.isPlace())
+		    			  test.ispossible();
+		    			  else test.isimpossible();
+		    			  
+		    		  }
+		    		  if(test.getLigne() == x && test.getCol() == y-1){
+		    			  if(!test.isPlace())
+		    			  test.ispossible();
+		    			  else test.isimpossible();
+		    		  }
+		    		  if(test.getLigne() == x-1 && test.getCol() == y-1){
+		    			  if(!test.isPlace())
+		    			  test.ispossible();
+		    			  else test.isimpossible();
+		    		  }
+		    		  if(test.getLigne() == x-1 && test.getCol() == y){
+		    			  if(!test.isPlace())
+		    			  test.ispossible();
+		    			  else test.isimpossible();
+		    		  } 
+		    	  }
+		      }
+		      else{
+		    	  pionSelectionne=null;
+		    	  for(Case test : grille){
+			        	test.resetColor();
+			        }
+		      }
+		    }
+		  }
+		  else
+		      if(o instanceof Case && pionSelectionne!=null)
+		      {
+		        Case r = (Case)o;
+		        if(r.getEtat() == Etat.possible){
+		        	int x = pionSelectionne.getLigne();
+		        	int y = pionSelectionne.getCol();
+		        	
+		        //le nouveau centre du jeton sera au centre du rectangle sélectionné
+		        double newX =  r.getX()  + tailleCase/2;
+		        double newY =  r.getY() + tailleCase/2;
+		        int tps = 300;
+		        pionSelectionne.switchSelected();
+		        Timeline timeline = new Timeline();
+		        timeline.getKeyFrames().addAll(
+		          new KeyFrame(new Duration(tps), 
+		                new KeyValue(pionSelectionne.centerXProperty(), newX),
+		                new KeyValue(pionSelectionne.centerYProperty(), newY),
+		                new KeyValue(pionSelectionne.fillProperty(), pionSelectionne.getCj())
+		                ));
+		        timeline.play();
+		        pionSelectionne.setLigne(r.getLigne());
+		        pionSelectionne.setCol(r.getCol());
+		        for(Case test : grille){
+		        	if(test.getLigne() == x && test.getCol() == y){
+		        		test.supprPion();
+		        	}
+		        	if(test.getLigne() == pionSelectionne.getLigne() && test.getCol() == pionSelectionne.getCol()){
+		        		test.placePion();
+		        	}
+		        	test.resetColor();
+		        }
+
+		        pionSelectionne = null;
+		      }
+		   }
+		}
 	
 }
--- src/main/Appli.java
+++ src/main/Appli.java
@@ -5,21 +5,14 @@ import java.util.ArrayList;
 import Entite.Case;
 import Entite.Etat;
 import Entite.Joueur;
-import Entite.Pion;
 import Entite.Plateau;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
-public class Appli extends Application implements EventHandler<MouseEvent> {
+public class Appli extends Application {
 	/** taille d'une case en pixels */
 	int tailleCase;
 	int nbLigne;
@@ -28,7 +21,6 @@ public class Appli extends Application implements EventHandler<MouseEvent> {
 	int decalage;
 	boolean rejouer = false;
 	
-	Pion pionSelectionne;
 	ArrayList<Case> grille = new ArrayList();
 
 	Joueur J1 = new Joueur(Etat.J1, true);
@@ -114,111 +106,5 @@ public class Appli extends Application implements EventHandler<MouseEvent> {
 	}
 
 	/**reponse aux evenements de souris*/
-	public void handle(MouseEvent me){
-	  
-	  Object o = me.getSource();
-	  if(o instanceof Pion)
-	  {
-	    Pion d = (Pion)o;
-
-        System.out.println("clic en " + d.getLigne() + "," + d.getCol());
-	    //si on selectionne un nouveau pion ou deselectionne le pion deja selectionne
-	    if(pionSelectionne==null || pionSelectionne==d)
-	    {
-	      d.switchSelected();
-	      d.colorPion();
-	      if(pionSelectionne==null){ 
-	    	  pionSelectionne=d;
-	    	  int x = pionSelectionne.getLigne();
-	    	  int y = pionSelectionne.getCol();
-	    	  for(Case test : grille){
-	    		  if(test.getLigne() == x+1 && test.getCol() == y+1){
-	    			  if(!test.isPlace())
-	    			  test.ispossible();
-	    			  else test.isimpossible();
-	    		  }
-	    		  if(test.getLigne() == x && test.getCol() == y+1){
-	    			  if(!test.isPlace())
-	    			  test.ispossible();
-	    			  else test.isimpossible();
-	    		  }
-	    		  if(test.getLigne() == x-1 && test.getCol() == y+1){
-	    			  if(!test.isPlace())
-	    			  test.ispossible();
-	    			  else test.isimpossible();
-	    		  }
-	    		  if(test.getLigne() == x+1 && test.getCol() == y){
-	    			  if(!test.isPlace())
-	    			  test.ispossible();
-	    			  else test.isimpossible();
-	    		  }
-	    		  if(test.getLigne() == x+1 && test.getCol() == y-1){
-	    			  if(!test.isPlace())
-	    			  test.ispossible();
-	    			  else test.isimpossible();
-	    			  
-	    		  }
-	    		  if(test.getLigne() == x && test.getCol() == y-1){
-	    			  if(!test.isPlace())
-	    			  test.ispossible();
-	    			  else test.isimpossible();
-	    		  }
-	    		  if(test.getLigne() == x-1 && test.getCol() == y-1){
-	    			  if(!test.isPlace())
-	    			  test.ispossible();
-	    			  else test.isimpossible();
-	    		  }
-	    		  if(test.getLigne() == x-1 && test.getCol() == y){
-	    			  if(!test.isPlace())
-	    			  test.ispossible();
-	    			  else test.isimpossible();
-	    		  } 
-	    	  }
-	      }
-	      else{
-	    	  pionSelectionne=null;
-	    	  for(Case test : grille){
-		        	test.resetColor();
-		        }
-	      }
-	    }
-	  }
-	  else
-	      if(o instanceof Case && pionSelectionne!=null)
-	      {
-	        Case r = (Case)o;
-	        if(r.getEtat() == Etat.possible){
-	        	int x = pionSelectionne.getLigne();
-	        	int y = pionSelectionne.getCol();
-	        	
-	        //le nouveau centre du jeton sera au centre du rectangle sélectionné
-	        double newX =  r.getX()  + tailleCase/2;
-	        double newY =  r.getY() + tailleCase/2;
-	        int tps = 300;
-	        pionSelectionne.switchSelected();
-	        Timeline timeline = new Timeline();
-	        timeline.getKeyFrames().addAll(
-	          new KeyFrame(new Duration(tps), 
-	                new KeyValue(pionSelectionne.centerXProperty(), newX),
-	                new KeyValue(pionSelectionne.centerYProperty(), newY),
-	                new KeyValue(pionSelectionne.fillProperty(), pionSelectionne.getCj())
-	                ));
-	        timeline.play();
-	        pionSelectionne.setLigne(r.getLigne());
-	        pionSelectionne.setCol(r.getCol());
-	        for(Case test : grille){
-	        	if(test.getLigne() == x && test.getCol() == y){
-	        		test.supprPion();
-	        	}
-	        	if(test.getLigne() == pionSelectionne.getLigne() && test.getCol() == pionSelectionne.getCol()){
-	        		test.placePion();
-	        	}
-	        	test.resetColor();
-	        }
-
-	        pionSelectionne = null;
-	      }
-	   }
-	}
 
 }
