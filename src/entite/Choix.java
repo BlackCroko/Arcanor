3c86fbf02e619bdb005cfbe49c9b88cecab5492b
blackCroko <rthuillez59@gmail.com>
2018-04-03 11:31:05 +0200

    affichage bouton

--- src/entite/Choix.java
+++ src/entite/Choix.java
@@ -1,36 +1,52 @@
 package entite;
 
+import javax.swing.ImageIcon;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class Choix extends Rectangle{
+public class Choix extends ImageView{
 	
+	private Image img1;
+	private Image img1S;
+	private Image img2;
+	private Image img2S;
 	private int id;
 	private Color Libre = Color.GREY;
 	private Color select = Color.DARKGOLDENROD;
 	private boolean selected = false;
 
 	public Choix(int id, int tailleCase) {
-		super(100, tailleCase*8, 150, 50);
+		this.id = id;
+		init();
 		if(id == 0){
 			selected = true;
+			this.setImage(img1S);
 			this.setX(100);
-			this.setFill(select);
+			this.setY(tailleCase*8);
 		}
 		else{
+			this.setImage(img2);
 			this.setX(tailleCase*6);
-			this.setFill(Libre);
+			this.setY(tailleCase*8);
 		}
 	}
 	
 	public void switchColor(){
 		if(selected){
-			this.setFill(Libre);
 			selected = false;
+			if(id == 0)
+			this.setImage(img1);
+			else
+				this.setImage(img2);
 		}
 		else{
-			this.setFill(select);
 			selected = true;
+			if(id == 0)
+				this.setImage(img1S);
+			else
+				this.setImage(img2S);
 		}
 	}
 
@@ -38,5 +54,11 @@ public class Choix extends Rectangle{
 		return selected;
 	}
 	
+	public void init(){
+		img1 = new Image("Choix1.png");
+		img1S = new Image("Choix1S.png");
+		img2 = new Image("Choix2.png");
+		img2S = new Image("Choix2S.png");
+	}
 	
 }
