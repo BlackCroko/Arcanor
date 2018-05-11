2539191984327dddd78a32535efcfd84d03d5fbb
adoublard <31312065+adoublard@users.noreply.github.com>
2018-05-11 18:20:35 +0200

    indent

--- src/entite/Grille.java
+++ src/entite/Grille.java
@@ -1,10 +1,10 @@
 package entite;
 
 public class Grille {
-	
+
 	Case[][] grille = new Case[8][7];
-	
-	public Grille(Case[][] grille){
+
+	public Grille(Case[][] grille) {
 		this.grille = grille;
 	}
 
@@ -15,8 +15,8 @@ public class Grille {
 	public void setGrille(Case[][] grille) {
 		this.grille = grille;
 	}
-	
-	public Grille deplacement(int x1, int y1, int x2, int y2){
+
+	public Grille deplacement(int x1, int y1, int x2, int y2) {
 		return this;
 	}
 
--- src/entite/IA.java
+++ src/entite/IA.java
@@ -3,69 +3,67 @@ package entite;
 import java.util.ArrayList;
 
 public class IA {
-	
 
 	Case[][] grille = new Case[8][7];
 	ArrayList<Grille> fils = new ArrayList<Grille>();
 	Grille G = new Grille(grille);
 
-		public IA(Case[][] grille){
-			this.grille = grille;
-			for (int k = 0; k < grille.length; k++) {
-				System.out.println("+-+-+-+-+-+-+-+");
-				for (int l = 0; l < grille[k].length; l++) {
-					if (grille[k][l].Contenu() != null) {
-						if (grille[k][l].Contenu().getJoueur() == 1) {
-							int x = grille[k][l].Contenu().getLigne();
-							int y = grille[k][l].Contenu().getCol();
-							for (int i = 0; i < grille.length; i++) {
-								for (int j = 0; j < grille[i].length; j++) {
+	public IA(Case[][] grille) {
+		this.grille = grille;
+		for (int k = 0; k < grille.length; k++) {
+			System.out.println("+-+-+-+-+-+-+-+");
+			for (int l = 0; l < grille[k].length; l++) {
+				if (grille[k][l].Contenu() != null) {
+					if (grille[k][l].Contenu().getJoueur() == 1) {
+						int x = grille[k][l].Contenu().getLigne();
+						int y = grille[k][l].Contenu().getCol();
+						for (int i = 0; i < grille.length; i++) {
+							for (int j = 0; j < grille[i].length; j++) {
 
-									if (grille[i][j].getLigne() == x + 1 && grille[i][j].getCol() == y + 1) {
-										if (grille[i][j].isJouable(grille[k][l].Contenu()))
-											fils.add(G.deplacement(i,j,x+1,y+1));
-									}
-									if (grille[i][j].getLigne() == x && grille[i][j].getCol() == y + 1) {
-										if (grille[i][j].isJouable(grille[k][l].Contenu()))
-											fils.add(G.deplacement(i,j,x,y+1));
-									}
-									if (grille[i][j].getLigne() == x - 1 && grille[i][j].getCol() == y + 1) {
-										if (grille[i][j].isJouable(grille[k][l].Contenu()))
-											fils.add(G.deplacement(i,j,x-1,y+1));
-									}
-									if (grille[i][j].getLigne() == x + 1 && grille[i][j].getCol() == y) {
-										if (grille[i][j].isJouable(grille[k][l].Contenu()))
-											fils.add(G.deplacement(i,j,x+1,y));
-									}
-									if (grille[i][j].getLigne() == x + 1 && grille[i][j].getCol() == y - 1) {
-										if (grille[i][j].isJouable(grille[k][l].Contenu()))
-											fils.add(G.deplacement(i,j,x+1,y-1));
-									}
-									if (grille[i][j].getLigne() == x && grille[i][j].getCol() == y - 1) {
-										if (grille[i][j].isJouable(grille[k][l].Contenu()))
-											fils.add(G.deplacement(i,j,x,y-1));
-									}
-									if (grille[i][j].getLigne() == x - 1 && grille[i][j].getCol() == y - 1) {
-										if (grille[i][j].isJouable(grille[k][l].Contenu()))
-											fils.add(G.deplacement(i,j,x-1,y-1));
-									}
-									if (grille[i][j].getLigne() == x - 1 && grille[i][j].getCol() == y) {
-										if (grille[i][j].isJouable(grille[k][l].Contenu()))
-											fils.add(G.deplacement(i,j,x-1,y));
-									}
+								if (grille[i][j].getLigne() == x + 1 && grille[i][j].getCol() == y + 1) {
+									if (grille[i][j].isJouable(grille[k][l].Contenu()))
+										fils.add(G.deplacement(i, j, x + 1, y + 1));
+								}
+								if (grille[i][j].getLigne() == x && grille[i][j].getCol() == y + 1) {
+									if (grille[i][j].isJouable(grille[k][l].Contenu()))
+										fils.add(G.deplacement(i, j, x, y + 1));
+								}
+								if (grille[i][j].getLigne() == x - 1 && grille[i][j].getCol() == y + 1) {
+									if (grille[i][j].isJouable(grille[k][l].Contenu()))
+										fils.add(G.deplacement(i, j, x - 1, y + 1));
+								}
+								if (grille[i][j].getLigne() == x + 1 && grille[i][j].getCol() == y) {
+									if (grille[i][j].isJouable(grille[k][l].Contenu()))
+										fils.add(G.deplacement(i, j, x + 1, y));
+								}
+								if (grille[i][j].getLigne() == x + 1 && grille[i][j].getCol() == y - 1) {
+									if (grille[i][j].isJouable(grille[k][l].Contenu()))
+										fils.add(G.deplacement(i, j, x + 1, y - 1));
+								}
+								if (grille[i][j].getLigne() == x && grille[i][j].getCol() == y - 1) {
+									if (grille[i][j].isJouable(grille[k][l].Contenu()))
+										fils.add(G.deplacement(i, j, x, y - 1));
+								}
+								if (grille[i][j].getLigne() == x - 1 && grille[i][j].getCol() == y - 1) {
+									if (grille[i][j].isJouable(grille[k][l].Contenu()))
+										fils.add(G.deplacement(i, j, x - 1, y - 1));
+								}
+								if (grille[i][j].getLigne() == x - 1 && grille[i][j].getCol() == y) {
+									if (grille[i][j].isJouable(grille[k][l].Contenu()))
+										fils.add(G.deplacement(i, j, x - 1, y));
 								}
 							}
 						}
-						System.out.print("|" + (grille[k][l].Contenu().getJoueur() + 1));
-					} else
-						System.out.print("|0");
-				}
-				System.out.println("|");
+					}
+					System.out.print("|" + (grille[k][l].Contenu().getJoueur() + 1));
+				} else
+					System.out.print("|0");
 			}
-			System.out.println("+-+-+-+-+-+-+-+");
-			System.out.println();
-			System.out.println();
+			System.out.println("|");
 		}
-
+		System.out.println("+-+-+-+-+-+-+-+");
+		System.out.println();
+		System.out.println();
 	}
 
+}
