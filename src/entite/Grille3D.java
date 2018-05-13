df66614e85efb50cf53a28f71024fa4ac2903dad
adoublard <31312065+adoublard@users.noreply.github.com>
2018-05-13 17:43:17 +0200

    isJouable3D

--- src/entite/Grille3D.java
+++ src/entite/Grille3D.java
@@ -69,7 +69,7 @@ public class Grille3D {
 	}
 
 	public boolean isJouable(int x1, int y1, int x2, int y2, int Joueur) {
-		if((x2 >=0 && x2 <8) || (y2 >=0 && y2 <7))
+		if((x2 < 0 && x2 >= 8) || (y2 < 0 && y2 >= 7))
 			return false;
 		if (grille[x2][y2][0] == 0 || (Joueur == 0 && grille[x2][y2][0] - 4 == grille[x1][y1][0] + 1)
 				|| (Joueur == 1 && grille[x2][y2][0] + 1 == grille[x1][y1][0] - 4))
