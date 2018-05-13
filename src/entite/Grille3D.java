b878d2237a0fedd92593838af118fcebaa6713b3
adoublard <31312065+adoublard@users.noreply.github.com>
2018-05-13 18:25:43 +0200

    Score3D

--- src/entite/Grille3D.java
+++ src/entite/Grille3D.java
@@ -38,18 +38,27 @@ public class Grille3D {
 		}
 	}
 
-	public void deplacement(int x1, int y1, int x2, int y2, boolean extraire) {
-		if (extraire || grille[x2][y2][0] != 0) {
-			descente(x2, y2);
-			grille[x2][y2][0] = grille[x1][y1][0];
-			remonter(x1, y1);
-		} else {
-			for (int i = 0; i < 4; i++) {
-				grille[x2][y2][i] = grille[x1][y1][i];
-				grille[x1][y1][i] = 0;
-			}
-		}
-	}
+	public Grille3D deplacement(int x1, int y1, int x2, int y2, boolean extraire) {
+        Grille3D g = null;
+        try {
+            g = (Grille3D) this.clone();
+        } catch (CloneNotSupportedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        if (extraire || g.getGrille()[x2][y2][0] != 0) {
+            descente(x2, y2);
+            g.getGrille()[x2][y2][0] = g.getGrille()[x1][y1][0];
+            remonter(x1, y1);
+        } else {
+            for (int i = 0; i < 4; i++) {
+                g.getGrille()[x2][y2][i] = g.getGrille()[x1][y1][i];
+                g.getGrille()[x1][y1][i] = 0;
+            }
+        }
+        g.affichage();
+        return g;
+    }
 
 	public void remonter(int x, int y) {
 		for (int i = 0; i < 3; i++) {
@@ -68,8 +77,25 @@ public class Grille3D {
 		return grille;
 	}
 
+	public int score(int Joueur) {
+		int cpt = 0;
+		int i;
+		if (Joueur == 0)
+			i = 7;
+		else
+			i = 0;
+
+		for (int j = 0; j < 8; j++) {
+			if (grille[i][j][0] < 5 && Joueur == 0)
+				cpt += grille[i][j][0];
+			if (grille[i][j][0] > 4 && Joueur == 1)
+				cpt += (grille[i][j][0] - 4);
+		}
+		return cpt;
+	}
+
 	public boolean isJouable(int x1, int y1, int x2, int y2, int Joueur) {
-		if((x2 < 0 || x2 >= 8) || (y2 < 0 || y2 >= 7))
+		if ((x2 < 0 || x2 >= 8) || (y2 < 0 || y2 >= 7))
 			return false;
 		if (grille[x2][y2][0] == 0 || (Joueur == 0 && grille[x2][y2][0] - 4 == grille[x1][y1][0] + 1)
 				|| (Joueur == 1 && grille[x2][y2][0] + 1 == grille[x1][y1][0] - 4))
