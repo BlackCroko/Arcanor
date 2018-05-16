f9fa256562921d68eaee94ae632cbe2497a0d68f
adoublard <31312065+adoublard@users.noreply.github.com>
2018-05-16 15:21:44 +0200

    scoreCache

--- src/entite/Heuristique.java
+++ src/entite/Heuristique.java
@@ -1,20 +1,33 @@
 package entite;
 
 public class Heuristique {
-	
+
 	public static final double MAX_NOTE = Double.MAX_VALUE;
-	
+
 	public static final double MIN_NOTE = Double.MIN_VALUE;
 
 	public double noteGrille(int[][][] grille, int joueur) {
 		return Math.random();
 	}
-	
-	public double scoredistance(int[][][] grille, int joueur){
-		//TODO point en fonction de la distance a la ligne adverse
-		// exemple a 1case tu marque 10 pts, a 2 cases 8 etc apres tu cherches pour avoir des points equilibré
-		
-		return 0;
+
+	public double scoredistance(int[][][] grille, int joueur) {
+		// TODO point en fonction de la distance a la ligne adverse
+		// exemple a 1case tu marque 10 pts, a 2 cases 8 etc apres tu cherches pour
+		// avoir des points equilibré ++ dernière ligne et regarder si chemin est libre
+		// ou s'il faut faire des détours
+		int cpt = 0;
+
+		for (int i = 0; i < grille.length; i++) {
+			for (int j = 0; j < grille[0].length; j++) {
+				if (j > 1 && grille[i][j][0] != 0)
+					if (joueur == 0 && grille[i][j][0] < 5 && grille[i][j][0] != 0) {
+						cpt += j * 2;
+						System.out.println(j);
+					}
+			}
+		}
+
+		return cpt;
 	}
 
 	public int score(int[][][] grille, int Joueur) {
@@ -25,7 +38,7 @@ public class Heuristique {
 		else
 			j = 0;
 
-		for (int i = 0; i < 7; i++) {
+		for (int i = 0; i < grille.length; i++) {
 			if (grille[i][j][0] < 5 && Joueur == 0)
 				cpt += grille[i][j][0];
 			if (grille[i][j][0] > 4 && Joueur == 1)
@@ -39,9 +52,9 @@ public class Heuristique {
 		for (int i = 0; i < grille.length; i++) {
 			for (int j = 0; j < grille[i].length; j++) {
 				for (int k = 1; k < grille[i][j].length; k++) {
-					if (grille[i][j][k] < 5 && Joueur == 0 && j != 6)
+					if (grille[i][j][0] < 5 && grille[i][j][0] < 5 && Joueur == 0 && j != 6)
 						cpt += grille[i][j][k];
-					if (grille[i][j][k] > 4 && Joueur == 1 && j != 0)
+					if (grille[i][j][0] > 4 && grille[i][j][k] > 4 && Joueur == 1 && j != 0)
 						cpt += (grille[i][j][k] - 4);
 				}
 			}
