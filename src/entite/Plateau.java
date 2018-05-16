9c9604632ae1e86982f7c30bcd5a841429fd1b6c
BlackCroko <rthuillez59@gmail.com>
2018-05-16 23:50:24 +0200

    bug deplacement resolu

--- src/entite/Plateau.java
+++ src/entite/Plateau.java
@@ -244,7 +244,6 @@ public class Plateau implements EventHandler<MouseEvent> {
 			joueurActuel = 0;
 		score.switchTrait(joueurActuel);
 		ordi = new IA(grille);
-		ordi.G.affichage2D();
 		Heuristique h = new Heuristique();
 		System.out.println();
 		if (IA) {
